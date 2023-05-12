27124e4d243b69f75aea29877f90d3d3503e7d82
Arif Uddin <arifunctg@gmail.com>
2023-05-12 18:21:02 +0600

    build: new build

--- dist/MixTagInput.d.ts
+++
@@ -1,5 +0,0 @@
-import './MixTagInput.css';
-import React from 'react';
-import type { MixTagInputProps, MixTagInputRef } from './MixTagInput.d';
-declare const MixTagInput: React.ForwardRefExoticComponent<MixTagInputProps & React.RefAttributes<MixTagInputRef>>;
-export default MixTagInput;
--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,1 +1,25 @@
-export * from './MixTagInput';
+import React, { HTMLAttributes } from 'react';
+
+type Tag = {
+  type: 'tag'
+  label: string
+  classes?: string
+}
+
+type MixInputValue = string | Tag
+
+interface MixInputProps extends HTMLAttributes<HTMLDivElement> {
+  value?: MixInputValue[]
+  multiline?: boolean
+  onChange?: (value: MixInputValue[]) => void
+}
+
+interface MixInputRef {
+  inputRef: HTMLDivElement | null
+  insertContent: (newContent: MixInputValue) => void
+  getValue: () => MixInputValue[]
+}
+
+declare const MixInput: React.ForwardRefExoticComponent<MixInputProps & React.RefAttributes<MixInputRef>>;
+
+export { MixInput as default };
