e99120aca6a3657cd5560d3f9309a6ba0ea18428
Arif Uddin <arifunctg@gmail.com>
2024-08-17 18:27:40 +0600

    fix: exported helpers

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 import MixInput from './MixInput'
+import { editorValueToMixInputValue, mixInputValueToEditorValue } from './utils'
 import type { MixInputProps, MixInputRef, MixInputValue, MixInputValues, Tag } from './MixInputType.d'
 export default MixInput
-
+export { editorValueToMixInputValue, mixInputValueToEditorValue }
 
 export type {
   MixInputProps,
