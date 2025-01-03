61f8696d93785c622327547e0c528ec133d030e4
Arif Uddin <arifunctg@gmail.com>
2025-01-03 19:08:12 +0600

    chore: move className to tag render level

--- src/Tag.tsx
+++ src/Tag.tsx
@@ -3,11 +3,17 @@ import React from 'react'
 
 export default function Tag(props: NodeViewProps) {
   const { tagView, tagClassName } = props.extension.options
-  const { label, ...restAttrs } = props.node.attrs
+  const { label, className, ...restAttrs } = props.node.attrs
   return (
     <>
-      <NodeViewWrapper className={`mi-tag ${tagClassName || ''}`} data-type="tag" as="span">
-        {tagView ? tagView(props) : <span {...restAttrs}>{label}</span>}
+      <NodeViewWrapper data-type="tag" as="span">
+        {tagView ? (
+          tagView(props)
+        ) : (
+          <span className={`${tagClassName || ''} ${className || ''}`} {...restAttrs}>
+            {label}
+          </span>
+        )}
       </NodeViewWrapper>
       {'\u200B'}
     </>
