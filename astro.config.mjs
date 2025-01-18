ee98415ae97e68c396214927f41849ae57fb65a4
Arif Uddin <arifunctg@gmail.com>
2025-01-18 23:15:02 +0600

    fix: base for gh pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
+  base:'/react-mix-tag-input/',
   integrations: [react(), tailwind({
     applyBaseStyles: false,
   })]
